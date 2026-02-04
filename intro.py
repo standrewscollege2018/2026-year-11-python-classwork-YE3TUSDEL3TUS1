c1ff4ab6a6667abead610c3d820d2af0eef9771f
YE3TUSDEL3TUS1 <bha9957@stacmail.net>
2026-02-05 12:34:24 +1300

    Update intro.py

--- intro.py
+++ intro.py
@@ -6,3 +6,6 @@ print(123)
 print("helloworld")
 
 
+name = input("What is your name?")
+print(f"Hello {name}, it has been scientifically proven by a study conducted by the Massachusett's Institute of Technology that the name {name} has an average iq less than 40. Consider changing your name by spending 1000 robux and purchasing 'change username' gamepass")
+      
