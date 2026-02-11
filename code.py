09dc6accdd35428b32d051692c52671186d7fc9a
YE3TUSDEL3TUS1 <bha9957@stacmail.net>
2026-02-12 12:31:32 +1300

    Update code.py
    if statement

--- code.py
+++ code.py
@@ -1,7 +1,10 @@
+'''Calculate dosage of drugs'''
+
 age=int(input("What is your age?"))
 weight=int(input("What is your weight?"))
 if age<=11: 
-    print:(f"recommend {weight*10} mg paracetamol")
-else: print:("Recommend 1000 mg paracetamol (2 tablets)")
-
-    
+    if 50>weight>25:
+        print(f"Take {weight*10} mg paracetamol")
+    else: print("You are at an unhealthy weight")
+elif 100<weight or 35>weight: print("You are at an unhealthy weight")
+else: print("Take 1000 mg paracetamol (2 tablets)")
