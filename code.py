8258fe236a4ab78efe25915062ac35fbc1ba133f
YE3TUSDEL3TUS1 <bha9957@stacmail.net>
2026-02-12 12:02:47 +1300

    Create code.py

---
+++ code.py
@@ -0,0 +1,7 @@
+age=int(input("What is your age?"))
+weight=int(input("What is your weight?"))
+if age<=11: 
+    print:(f"recommend {weight*10} mg paracetamol")
+else: print:("Recommend 1000 mg paracetamol (2 tablets)")
+
+    
